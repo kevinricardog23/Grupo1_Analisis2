da5f8b20d1b19a633db0562c99ebb91a70a767a3
eddylo1995 <41241710+eddylo1995@users.noreply.github.com>
2018-07-17 19:33:52 -0600

    //holis

--- prototipo01/forms/Dashboard.cs
+++ prototipo01/forms/Dashboard.cs
@@ -59,7 +59,7 @@ namespace prototipo01.forms
         {
             openForm(new listaSecciones());
         }
-
+        //el
         private void button5_Click(object sender, EventArgs e)
         {
             openForm(new listaLaboratorios());
