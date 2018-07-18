0a44e55a25254c5d892d1ce9ff9a2d7f0b0a31eb
KEVIN <KEVIN@DESKTOP-BQ3SKDG>
2018-07-17 21:42:21 -0600

    a

---
+++ prototipo01/forms/seccion/reportes/CursosAlumnos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prototipo01.forms.seccion.reportes
+{
+    public partial class CursosAlumnos : Form
+    {
+        public CursosAlumnos()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
