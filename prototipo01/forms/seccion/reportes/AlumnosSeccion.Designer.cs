bbb0aa77244f288c65ebdbe1073f5b9ead2405d6
KEVIN RICARDO <kriicardoo@gmail.com>
2018-07-21 22:59:40 -0600

    validacion_campos

--- prototipo01/forms/seccion/reportes/AlumnosSeccion.Designer.cs
+++ prototipo01/forms/seccion/reportes/AlumnosSeccion.Designer.cs
@@ -194,6 +194,7 @@
             this.button1.TabIndex = 31;
             this.button1.Text = "BUSCAR";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button3
             // 
