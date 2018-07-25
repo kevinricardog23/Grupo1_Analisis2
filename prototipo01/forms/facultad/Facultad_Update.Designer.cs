402971f6073ac890d93dcd4e47e0c83adb573914
arrecis59 <arrecis59@gmail.com>
2018-07-25 00:11:38 -0600

    facultad UI

--- prototipo01/forms/facultad/Facultad_Update.Designer.cs
+++ prototipo01/forms/facultad/Facultad_Update.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace prototipo01
+﻿namespace prototipo01.forms.facultad
 {
-    partial class Laboratorio_Update
+    partial class Facultad_Update
     {
         /// <summary>
         /// Required designer variable.
@@ -28,19 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Laboratorio_Update));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Facultad_Update));
             this.CRUD = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.Btn_actualizar = new System.Windows.Forms.Button();
-            this.Cbo_salon = new System.Windows.Forms.ComboBox();
-            this.Cbo_catedratico = new System.Windows.Forms.ComboBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.Txt_nombre = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.CRUD.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.panel1.SuspendLayout();
@@ -51,18 +53,20 @@
             // 
             this.CRUD.Controls.Add(this.tabPage1);
             this.CRUD.Font = new System.Drawing.Font("Century Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.CRUD.Location = new System.Drawing.Point(103, 114);
+            this.CRUD.Location = new System.Drawing.Point(60, 122);
             this.CRUD.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.CRUD.Name = "CRUD";
             this.CRUD.SelectedIndex = 0;
             this.CRUD.Size = new System.Drawing.Size(1145, 586);
-            this.CRUD.TabIndex = 5;
+            this.CRUD.TabIndex = 7;
             // 
             // tabPage1
             // 
             this.tabPage1.Controls.Add(this.Btn_actualizar);
-            this.tabPage1.Controls.Add(this.Cbo_salon);
-            this.tabPage1.Controls.Add(this.Cbo_catedratico);
+            this.tabPage1.Controls.Add(this.textBox3);
+            this.tabPage1.Controls.Add(this.label5);
+            this.tabPage1.Controls.Add(this.textBox2);
+            this.tabPage1.Controls.Add(this.textBox1);
             this.tabPage1.Controls.Add(this.Txt_nombre);
             this.tabPage1.Controls.Add(this.label4);
             this.tabPage1.Controls.Add(this.label3);
@@ -82,73 +86,93 @@
             this.Btn_actualizar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.Btn_actualizar.Font = new System.Drawing.Font("Century Gothic", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Btn_actualizar.ForeColor = System.Drawing.Color.White;
-            this.Btn_actualizar.Location = new System.Drawing.Point(589, 442);
+            this.Btn_actualizar.Location = new System.Drawing.Point(618, 429);
             this.Btn_actualizar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Btn_actualizar.Name = "Btn_actualizar";
             this.Btn_actualizar.Size = new System.Drawing.Size(135, 59);
-            this.Btn_actualizar.TabIndex = 21;
+            this.Btn_actualizar.TabIndex = 26;
             this.Btn_actualizar.Text = "Actualizar";
             this.Btn_actualizar.UseVisualStyleBackColor = false;
-            this.Btn_actualizar.Click += new System.EventHandler(this.Btn_actualizar_Click_1);
             // 
-            // Cbo_salon
+            // textBox3
             // 
-            this.Cbo_salon.FormattingEnabled = true;
-            this.Cbo_salon.Location = new System.Drawing.Point(484, 319);
-            this.Cbo_salon.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.Cbo_salon.Name = "Cbo_salon";
-            this.Cbo_salon.Size = new System.Drawing.Size(257, 29);
-            this.Cbo_salon.TabIndex = 20;
+            this.textBox3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.textBox3.Location = new System.Drawing.Point(471, 333);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(421, 34);
+            this.textBox3.TabIndex = 25;
             // 
-            // Cbo_catedratico
+            // label5
             // 
-            this.Cbo_catedratico.FormattingEnabled = true;
-            this.Cbo_catedratico.Location = new System.Drawing.Point(484, 230);
-            this.Cbo_catedratico.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.Cbo_catedratico.Name = "Cbo_catedratico";
-            this.Cbo_catedratico.Size = new System.Drawing.Size(257, 29);
-            this.Cbo_catedratico.TabIndex = 19;
-            this.Cbo_catedratico.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Cbo_edificio_KeyPress);
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.label5.Location = new System.Drawing.Point(186, 344);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 23);
+            this.label5.TabIndex = 24;
+            this.label5.Text = "Correo";
+            // 
+            // textBox2
+            // 
+            this.textBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.textBox2.Location = new System.Drawing.Point(471, 249);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(421, 34);
+            this.textBox2.TabIndex = 23;
+            // 
+            // textBox1
+            // 
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.textBox1.Location = new System.Drawing.Point(471, 165);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(421, 34);
+            this.textBox1.TabIndex = 22;
             // 
             // Txt_nombre
             // 
             this.Txt_nombre.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
-            this.Txt_nombre.Location = new System.Drawing.Point(484, 138);
+            this.Txt_nombre.Location = new System.Drawing.Point(471, 83);
             this.Txt_nombre.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Txt_nombre.Multiline = true;
             this.Txt_nombre.Name = "Txt_nombre";
             this.Txt_nombre.Size = new System.Drawing.Size(421, 34);
             this.Txt_nombre.TabIndex = 16;
-            this.Txt_nombre.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Txt_nombre_KeyPress);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
-            this.label4.Location = new System.Drawing.Point(199, 236);
+            this.label4.Location = new System.Drawing.Point(186, 176);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(161, 23);
+            this.label4.Size = new System.Drawing.Size(104, 23);
             this.label4.TabIndex = 18;
-            this.label4.Text = "DPI catedratico";
+            this.label4.Text = "Dirección";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
-            this.label3.Location = new System.Drawing.Point(199, 325);
+            this.label3.Location = new System.Drawing.Point(186, 260);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(88, 23);
+            this.label3.Size = new System.Drawing.Size(93, 23);
             this.label3.TabIndex = 17;
-            this.label3.Text = "ID salon";
+            this.label3.Text = "Telefono";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
-            this.label2.Location = new System.Drawing.Point(199, 139);
+            this.label2.Location = new System.Drawing.Point(190, 94);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(89, 23);
             this.label2.TabIndex = 15;
@@ -159,22 +183,11 @@
             this.panel1.BackColor = System.Drawing.Color.WhiteSmoke;
             this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(103, 14);
+            this.panel1.Location = new System.Drawing.Point(60, 22);
             this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(1168, 59);
-            this.panel1.TabIndex = 4;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Century Gothic", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
-            this.label1.Location = new System.Drawing.Point(11, 20);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(215, 23);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Actualizar Laboratorio";
+            this.panel1.TabIndex = 6;
             // 
             // pictureBox1
             // 
@@ -187,7 +200,18 @@
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
-            // Laboratorio_Update
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.label1.Location = new System.Drawing.Point(11, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(191, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Actualizar Facultad";
+            // 
+            // Facultad_Update
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -196,11 +220,8 @@
             this.Controls.Add(this.CRUD);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.Name = "Laboratorio_Update";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Laboratorio_Update";
-            this.Load += new System.EventHandler(this.Laboratorio_Update_Load);
+            this.Name = "Facultad_Update";
+            this.Text = "Facultad_Update";
             this.CRUD.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.tabPage1.PerformLayout();
@@ -215,15 +236,17 @@
 
         private System.Windows.Forms.TabControl CRUD;
         private System.Windows.Forms.TabPage tabPage1;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox Cbo_catedratico;
         private System.Windows.Forms.TextBox Txt_nombre;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.ComboBox Cbo_salon;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Button Btn_actualizar;
     }
 }
