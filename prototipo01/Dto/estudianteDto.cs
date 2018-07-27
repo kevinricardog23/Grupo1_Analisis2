41604a753bcab32d04e5c713a231e6ab5d58389d
KEVIN RICARDO <kriicardoo@gmail.com>
2018-07-26 20:43:14 -0600

    Clases Controlador y Dto Alumnos

---
+++ prototipo01/Dto/estudianteDto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prototipo01.Dto
+{
+    class estudianteDto
+    {
+        public int dpi_alumno { get; set; }
+
+
+        public string nombre_alumno { get; set; }
+
+
+        public string apellido_alumno { get; set; }
+
+
+        public string telefono_alumno { get; set; }
+
+
+        public string correo_alumno { get; set; }
+
+        public int FACULTAD_id_facultad { get; set; }
+
+
+        public int CARRERA_id_carrera { get; set; }
+
+        public int edad_alumno { get; set; }
+
+        public string direccion_alumno { get; set; }
+
+        public string estado_alumno { get; set; }
+    }
+}
---
+++ prototipo01/controladores/ControladorEstudiante.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using prototipo01.models;
+using prototipo01.controladores;
+using prototipo01.Dto;
+
+namespace prototipo01.controladores
+{
+    class ControladorEstudiante
+    {
+        public BindingList<estudianteDto> Listado_estudiante()
+        {
+            try
+            {
+
+            }
+        }
+    }
+}
