626483e5ef10e3b3eb40caa4dfd08a7fcf45ea55
arrecis59 <arrecis59@gmail.com>
2018-08-02 08:46:53 -0600

    PRIV

--- prototipo01/Clases/privilegios.cs
+++ prototipo01/Clases/privilegios.cs
@@ -10,5 +10,17 @@ namespace prototipo01.Clases
     {
         public static int privilegio { get; set; }
 
+        public static Boolean bandera;
+
+        /*public static void openEdificios(int privilegio)
+        {
+            if (privilegio == 1)
+            {
+                bandera = true;
+            }
+
+            bandera = false;
+        }*/
+
     }
 }
