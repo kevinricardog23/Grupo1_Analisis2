9fce59506cb5ed3b1ffa8303519d44aba6fe0cef
Andrés Canú <ncanug@miumg.edu.gt>
2018-08-05 22:04:44 -0600

    Cambios en asignación

--- prototipo01/models/alumno.cs
+++ prototipo01/models/alumno.cs
@@ -39,6 +39,9 @@ namespace prototipo01.models
         public int edad_alumno { get; set; }
 
         [StringLength(45)]
+        public string carnet { get; set; }
+
+        [StringLength(45)]
         public string direccion_alumno { get; set; }
 
         [StringLength(45)]
